a9fe8e9838f979a21949e980860d9d1ade01a597
hernantos <rehsatart@gmail.com>
2021-10-11 23:26:30 +0300

    мне не нравится что я написал

---
+++ BadSmellingBotService.Tests/Mock/MockTelegramUpdateGenerator.cs
@@ -0,0 +1,17 @@
+﻿using BadSmellingBotService.Tests.Constants;
+using Telegram.Bot.Types;
+
+namespace BadSmellingBotService.Tests.Mock
+{
+    public class MockTelegramUpdateGenerator
+    {
+        public static Update Generate()
+            => new Update
+            {
+                Message = new Message
+                {
+                    Chat = new Chat()
+                }
+            };
+    }
+}
