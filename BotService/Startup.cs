fd107b52fe830ff0be088d9f03ebaf8ecb42f41c
Alexandr Dzhunushaliev <dzhunall@mail.ru>
2021-11-03 18:31:23 +0300

    b

--- BotService/Startup.cs
+++ BotService/Startup.cs
@@ -29,18 +29,18 @@ namespace BotService
             services.AddCommandHandlers();
 
             services.Configure<TelegramOptions>(Configuration.GetSection("Telegram"));
-            services.AddRabbitMQ(new Uri(""));
+            services.AddRabbitMQ(new Uri(Configuration.GetSection("Rabbit").GetValue<string>("Uri")));
             services.AddScoped<CommandsListService>();
             services.AddScoped<ConstantMessagesService>();
             services.AddScoped<UserRolesService>();
-            services.AddScoped<ICommandNotFoundHandler,NotCommandHandlersDispatcher>();
+            services.AddScoped<ICommandNotFoundHandler, NotCommandHandlersDispatcher>();
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             app.UseMiddleware<SaveUsersMiddleware>();
             app.UseMiddleware<SorryOlegMiddleware>();
-            
+
             app.UseCommandHandlers();
         }
     }
