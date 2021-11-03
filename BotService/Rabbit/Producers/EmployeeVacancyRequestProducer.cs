5be55203f75c6bf1cc9f18318c915c6c356de40d
Alexandr Dzhunushaliev <dzhunall@mail.ru>
2021-11-03 18:23:17 +0300

    b

---
+++ BotService/Rabbit/Producers/EmployeeVacancyRequestProducer.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using BotService.Models.Rabbit;
+using RabbitMQ.Services;
+
+namespace BotService.Rabbit.Producers
+{
+    public class EmployeeVacancyRequestProducer
+    {
+        private readonly IRabbitMQService _rabbitMqService;
+
+        public EmployeeVacancyRequestProducer(IRabbitMQService rabbitMqService)
+        {
+            _rabbitMqService = rabbitMqService;
+        }
+
+        public void Produce(EmployeeVacancyRequest dto)
+        {
+            _rabbitMqService.Publish(dto, "", "");
+        }
+    }
+}
